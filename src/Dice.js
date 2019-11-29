a0e40e53f43364194c5cd7ab92fab6d63e46489a
Kai Sawamoto <stayhumblekai@gmail.com>
2019-11-29 10:12:39 -0800

    updated np

--- src/Dice.js
+++ src/Dice.js
@@ -3,21 +3,23 @@ import Die from './Die';
 import './Dice.css';
 
 class Dice extends Component {
-  render() {
-    return <div className="Dice">
-      {this.props.dice.map((d, idx) =>
-        <Die handleClick={this.props.handleClick}
-          val={d}
-          locked={this.props.locked[idx]}
-          idx={idx}
-          key={idx}
-          disabled={this.props.disabled}
-          rolling={this.props.rolling}
-        />
-          
-      )}
-    </div>
-  }
+	render() {
+		return (
+			<div className="Dice">
+				{this.props.dice.map((d, idx) => (
+					<Die
+						handleClick={this.props.handleClick}
+						val={d}
+						locked={this.props.locked[idx]}
+						idx={idx}
+						key={idx}
+						disabled={this.props.disabled}
+						rolling={this.props.rolling}
+					/>
+				))}
+			</div>
+		);
+	}
 }
 
 export default Dice;
